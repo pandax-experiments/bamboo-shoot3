ca1ac78aca225b994c6191e3645624b5e96b4179
Carl Lei <xecycle@gmail.com>
2016-03-30 16:54:25 +0800

    bench: some mismatch patterns for optimistic parse

--- bench/bench-hitdata.cc
+++ bench/bench-hitdata.cc
@@ -152,6 +152,36 @@ int main()
         ;
     }
 
+    {
+      duration<uint64_t, std::nano> dur{0};
+      for (size_t isamp=0; isamp<NSAMPLES; ++isamp) {
+        auto start = clock.now();
+        (void)pbss::parse_from_buffer<HitData_tailadd>(out);
+        dur += clock.now() - start;
+      }
+      auto time = duration_cast<microseconds>(dur).count()/NSAMPLES;
+      cout << "tailadd parsed in "
+           << time << " us, "
+           << "real " << ((double)size / (1<<20)) / ((double)time / 1e6) << " MiB/s, "
+           << "effective " << ((double)valid_size(nhits) / (1<<20)) / ((double)time / 1e6) << " MiB/s\n"
+        ;
+    }
+
+    {
+      duration<uint64_t, std::nano> dur{0};
+      for (size_t isamp=0; isamp<NSAMPLES; ++isamp) {
+        auto start = clock.now();
+        (void)pbss::parse_from_buffer<HitData_mismatch>(out);
+        dur += clock.now() - start;
+      }
+      auto time = duration_cast<microseconds>(dur).count()/NSAMPLES;
+      cout << "mismatch parsed in "
+           << time << " us, "
+           << "real " << ((double)size / (1<<20)) / ((double)time / 1e6) << " MiB/s, "
+           << "effective " << ((double)valid_size(nhits) / (1<<20)) / ((double)time / 1e6) << " MiB/s\n"
+        ;
+    }
+
   }
   return 0;
 }
