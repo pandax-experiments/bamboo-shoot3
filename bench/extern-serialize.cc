17c7f806dc1854d978add45a348c6f8664fd2cf3
Carl Lei <xecycle@gmail.com>
2016-03-31 09:07:14 +0800

    bench: also extern _tailadd, _mismatch

--- bench/extern-serialize.cc
+++ bench/extern-serialize.cc
@@ -6,3 +6,5 @@
 
 template pbss::buffer pbss::serialize_to_buffer(const HitData&);
 template HitData pbss::parse_from_buffer<HitData>(const pbss::buffer&);
+template HitData_tailadd pbss::parse_from_buffer<HitData_tailadd>(const pbss::buffer&);
+template HitData_mismatch pbss::parse_from_buffer<HitData_mismatch>(const pbss::buffer&);
--- bench/extern-serialize.hh
+++ bench/extern-serialize.hh
@@ -3,5 +3,7 @@
 
 extern template pbss::buffer pbss::serialize_to_buffer(const HitData&);
 extern template HitData pbss::parse_from_buffer<HitData>(const pbss::buffer&);
+extern template HitData_tailadd pbss::parse_from_buffer<HitData_tailadd>(const pbss::buffer&);
+extern template HitData_mismatch pbss::parse_from_buffer<HitData_mismatch>(const pbss::buffer&);
 
 #endif /* BS3_BENCH_EXTERN_SERIALIZE_HH */
