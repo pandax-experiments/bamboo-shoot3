1b9cc7d1c6c22586b00d2a97c0ff0498f4c553ae
Carl Lei <xecycle@gmail.com>
2016-03-31 09:18:54 +0800

    bench: tune inline limit in build config

--- bench/extern-serialize.cc
+++ bench/extern-serialize.cc
@@ -2,8 +2,6 @@
 
 #include "hitdata.hh"
 
-#pragma GCC optimize("-finline-limit=1200")
-
 template pbss::buffer pbss::serialize_to_buffer(const HitData&);
 template HitData pbss::parse_from_buffer<HitData>(const pbss::buffer&);
 template HitData_tailadd pbss::parse_from_buffer<HitData_tailadd>(const pbss::buffer&);
